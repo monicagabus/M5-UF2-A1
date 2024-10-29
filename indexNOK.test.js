45b3e9269d8cf3c550ef2c3d90898f7c9de2f545
monicagabus <monicagabus@gmail.com>
2024-10-29 19:01:17 +0100

    caso 4 test

--- indexNOK.test.js
+++ indexNOK.test.js
@@ -5,5 +5,8 @@ describe("Función CalculaTipo", () => {
   test("Comprobación caso 5", () => {
     expect(CalculaTipo(1000, 0)).toBe(5);
   });
+  test('Comprobación caso 4', () => {
+    expect(CalculaTipo(1000, 400)).toBe(4);
+  });
   
 });
